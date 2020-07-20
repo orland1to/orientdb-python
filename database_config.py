8890104a3bd06e722ab9dbb955fb475cab14ab37
orlando <orlandosalvadorcamarillomoreno@gmail.com>
2020-07-20 14:11:25 -0500

    added files

--- database_config.py
+++ database_config.py
@@ -4,12 +4,12 @@ from socket_config import Socket
 import db_atributes
 socket = Socket("orientDB", 2424)
 try:
+    print("here")
     socket.connect()
     client = OrientDB(socket)
-    session_id = client.connect("root", "password")
-    client.db_open("test", "root", "password")
-    for item in db_atributes.orient_classes():
-            print("CREATE CLASS " + item['name'] + " IF NOT EXISTS EXTENDS "+item['classType'])
+    session_id = client.connect("root", "password")#get session id
+    client.db_open("test", "root", "password") #open db
+    for item in db_atributes.orient_classes():  #create a classes and atributes of new class if not exists based on db atributes file
             client.command(
                 "CREATE CLASS " + item['name'] + " IF NOT EXISTS EXTENDS "+item['classType']
             )
@@ -18,7 +18,10 @@ try:
             )
 except Exception as e:
     print(e)
-    if not client.db_exists("test"):
+    socket.connect()
+    client = OrientDB(socket)
+    session_id = client.connect("root", "password")#get session id
+    if not client.db_exists("test"):#if exeption is for non created db  lets create the db and classes with atributes based on db_atributes file
         client.db_create(
         "test",
         pyorient.DB_TYPE_GRAPH,
